baf2fe4c35e0a45c508c2a1959a9f111926e0656
Alex Yiu <alex13920.cep@gmail.com>
2021-07-04 21:56:36 +0800

    multiple cards

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,37 +1,77 @@
-import React, { useState, useEffect } from 'react';
-import getBlockchain from './ethereum.js';
-import axios from 'axios';
+import React from 'react'
+import Card from './Card';
+import { useState} from 'react';
 
-function App() {
-  const [tokenInfo, setTokenInfo] = useState(undefined);
+export default function App() {
+    return (
+        <div>
+            <div class="position-relative overflow-hidden p-3 p-md-5 m-md-3 text-center bg-light">
+                    <div class="col-md-5 p-lg-5 mx-auto my-5">
+                    <h1 class="display-4 fw-normal">Punny headline</h1>
+                    <p class="lead fw-normal">And an even wittier subheading to boot. Jumpstart your marketing efforts with this example based on Apple’s marketing pages.</p>
+                    <a class="btn btn-outline-secondary" href="#">Coming soon</a>
+                </div>
+                <div class="product-device shadow-sm d-none d-md-block"></div>
+                <div class="product-device product-device-2 shadow-sm d-none d-md-block"></div>
+            </div>
+            <div class="container">
+                <div class="row">
+                    <Card tokenId= {0}/>
+                    <Card tokenId= {1}/>
+                </div>
+            </div>
 
-  useEffect(() => {
-    const init = async () => {
-      const { nft } = await getBlockchain();
-      const tokenURI = await nft.tokenURI(0);
-      console.log(tokenURI);
-      const { data } = await axios.get(tokenURI);
-      setTokenInfo(data);
-    };
-    init();
-  }, []);
+            
+            <div class="d-md-flex flex-md-equal w-100 my-md-3 ps-md-3">
+                <div class="bg-light me-md-3 pt-3 px-3 pt-md-5 px-md-5 text-center overflow-hidden">
+                <div class="my-3 p-3">
+                    <h2 class="display-5">Another headline (second)</h2>
+                    <p class="lead">And an even wittier subheading.</p>
+                </div>
+                <div class="bg-dark shadow-sm mx-auto"></div>
+            </div>
+            <div class="bg-primary me-md-3 pt-3 px-3 pt-md-5 px-md-5 text-center text-white overflow-hidden">
+                <div class="my-3 py-3">
+                    <h2 class="display-5">Another headline</h2>
+                    <p class="lead">And an even wittier subheading.</p>
+                </div>
+                <div class="bg-light shadow-sm mx-auto" ></div>
+                </div>
+            </div>
 
-  if(typeof tokenInfo === 'undefined') {
-    return 'token 0 undefined';
-  }
+            <div class="d-md-flex flex-md-equal w-100 my-md-3 ps-md-3">
+                <div class="bg-light me-md-3 pt-3 px-3 pt-md-5 px-md-5 text-center overflow-hidden">
+                <div class="my-3 p-3">
+                    <h2 class="display-5">Another headline</h2>
+                    <p class="lead">And an even wittier subheading.</p>
+                </div>
+                <div class="bg-body shadow-sm mx-auto" ></div>
+                </div>
+                <div class="bg-light me-md-3 pt-3 px-3 pt-md-5 px-md-5 text-center overflow-hidden">
+                <div class="my-3 py-3">
+                    <h2 class="display-5">Another headline</h2>
+                    <p class="lead">And an even wittier subheading.</p>
+                </div>
+                <div class="bg-body shadow-sm mx-auto"></div>
+                </div>
+            </div>
 
-  return (
-    
-    <div className='container'>
-      <div class="bg-light me-md-3 pt-3 px-3 pt-md-5 px-md-5 text-center overflow-hidden">
-        <div class="my-3 p-3">
-          <h2 class="display-5">{"Tocken Name: "+ tokenInfo.name}</h2>
-          <p class="lead">{"Tocken Discription: "+ tokenInfo.description}</p>
-          <img src={tokenInfo.image} className="img-fluid" />
+            <div class="d-md-flex flex-md-equal w-100 my-md-3 ps-md-3">
+                <div class="bg-light me-md-3 pt-3 px-3 pt-md-5 px-md-5 text-center overflow-hidden">
+                <div class="my-3 p-3">
+                    <h2 class="display-5">Another headline</h2>
+                    <p class="lead">And an even wittier subheading.</p>
+                </div>
+                <div class="bg-body shadow-sm mx-auto"></div>
+                </div>
+                <div class="bg-light me-md-3 pt-3 px-3 pt-md-5 px-md-5 text-center overflow-hidden">
+                <div class="my-3 py-3">
+                    <h2 class="display-5">Another headline</h2>
+                    <p class="lead">And an even wittier subheading.</p>
+                </div>
+                <div class="bg-body shadow-sm mx-auto"></div>
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  );
+    )
 }
-
-export default App;
