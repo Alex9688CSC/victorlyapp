78254150903ed766a2a3d39d18f6f0e89495c31d
Alex Yiu <alex13920.cep@gmail.com>
2021-07-05 12:26:54 +0800

    created two collections
    update backend and contracts

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,8 +1,15 @@
 const NFT = artifacts.require("NFT");
+const PLAYERTWO = artifacts.require("PLAYERTWO");
 
 module.exports = async function (deployer, _network, accounts) {
   await deployer.deploy(NFT);
   const nft = await NFT.deployed();
   await nft.mint(accounts[0], 0);
   await nft.mint(accounts[0], 1);
+  //deploy player two (Hank)
+  await deployer.deploy(PLAYERTWO);
+  const playertwo = await PLAYERTWO.deployed();
+  await playertwo.mint(accounts[0], 0);
+  await playertwo.mint(accounts[0], 1);
+
 };
