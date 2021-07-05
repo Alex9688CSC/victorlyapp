d0bb323611dc3bd249649a3b3a1f59e391f8a713
Alex Yiu <alex13920.cep@gmail.com>
2021-07-05 14:44:00 +0800

    have two players now

---
+++ contracts/PLAYERONE.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+import '@openzeppelin/contracts/token/ERC721/ERC721.sol';
+
+contract PLAYERONE is Ownable, ERC721 {
+  uint public TokenId;
+  address public admin;
+
+  constructor() ERC721('Playerone NFT', 'PlayeroneNFT') {
+    admin = msg.sender;
+  }
+
+  function mint(address to, uint tokenid) external {
+    require(msg.sender == admin, 'only admin');
+    _safeMint(to, tokenid);
+    TokenId++;
+  }
+
+  function _baseURI() internal view override returns (string memory) {
+    return 'https://victorapi.herokuapp.com/api/playerone/';
+  }
+  
+
+}
--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,11 +1,17 @@
 const NFT = artifacts.require("NFT");
+const PLAYERONE = artifacts.require("PLAYERONE");
 const PLAYERTWO = artifacts.require("PLAYERTWO");
 
 module.exports = async function (deployer, _network, accounts) {
-  await deployer.deploy(NFT);
-  const nft = await NFT.deployed();
-  await nft.mint(accounts[0], 0);
-  await nft.mint(accounts[0], 1);
+  // await deployer.deploy(NFT);
+  // const nft = await NFT.deployed();
+  // await nft.mint(accounts[0], 0);
+  // await nft.mint(accounts[0], 1);
+  //deploy player one (Novak Djokovic)
+  await deployer.deploy(PLAYERONE);
+  const playerone = await PLAYERONE.deployed();
+  await playerone.mint(accounts[0], 0);
+  await playerone.mint(accounts[0], 1);
   //deploy player two (Hank)
   await deployer.deploy(PLAYERTWO);
   const playertwo = await PLAYERTWO.deployed();
